ae22a70245d7788200b4ec9c26853575b6e3ebb0
HATEM ABDALLA <hhatim@gmail.com>
2018-06-18 00:51:14 -0700

    initial commit

--- pages/qaHomePage.js
+++ pages/qaHomePage.js
@@ -8,31 +8,21 @@ class QaHomePage extends BasePage {
         this.url = 'http://cnn.com';
 
         // include modules...
-        //this.cnnLink = $('a#cnnLink');
+        this.cnnLink = $('a#cnnLink');
 
         // since the table happens to have multiple rows, the 5th row is clicked to show that there are at least 5 li links or rows:
+        // :nth-child(n+5) matches children starting from the 5th (inclusive)
         this.topStories = $('#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-2 > ul > li:nth-child(n+5)')
-       
+              
 
         this.StoriesList = $('#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-2 > ul')
-        //         console.log('--------  StoriesList ---------')
-        //         console.log('success')
-        //         return this.StoriesList;
 
 
-        // the first li link is clicked to show it has content and the link is functioning 
+        // the first ul link is checked to show it has content and the link is working 
         this.storyKicker = $('#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul > li:nth-child(1) >  article > div > div.cd__content')
-                   
-                  console.log('-------- StoryKicker ----------')
-                  //return  this.storykicker.innerText;
-                  //console.log(storyKicker.innerText)
-                  //var text = element.textContent;
-                  //element.textContent = "this is some sample text";
-                  
+                         
 
     }
-
-
     // async topStories(topStories) {
         
 
--- tests/qaTest.js
+++ tests/qaTest.js
@@ -7,31 +7,27 @@ fixture`CNN qaHomePage`
     .page`${qaHomePage.url}`
     .page`${cnnPage.url}`
 
-        test('1a. Assert that there are at least 5 top stories', async t => {
+
+        test('1. Assert that there are at least 5 top stories', async t => {
    
         await t.expect(qaHomePage.topStories.innerText != null);
-        //await t.expect(qaHomePage.topStories.exists).ok();
+        await t.expect(qaHomePage.topStories.exists).ok();
         //await t.expect(qaHomePage.topStories.innerText).contains('');
         await t.click(qaHomePage.topStories());
-    
-	});
 
-        test('1b. Assert that there are at least 5 top stories', async t => {
-     	
-        //.expect(list.count).eql(2)
-		await t.expect(qaHomePage.StoriesList.exists).ok();
-    	await t.expect(qaHomePage.StoriesList.innerText != null);
 	});
-
 
         test('2. Assert the kicker under the main story’s picture is filled out', async t => {
           
     	await t.click(qaHomePage.storyKicker());
     	await t.expect(qaHomePage.storyKicker.innerText != null);
-            console.log('kicker under the main story’s picture is filled out')
+            //console.log('kicker under the main story’s picture is filled out')
                
     });
 
-
+      // test('1b. Assert that there are at least 5 top stories', async t => {  
+       //await t.expect(qaHomePage.StoriesList.exists).ok();
+       //await t.expect(qaHomePage.StoriesList.innerText != null);
+    // });
 
 // testcafe chrome qaTest.js
