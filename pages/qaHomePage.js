a126ef49023d1f41116f602f3f410e5359024635
HATEM ABDALLA <hhatim@gmail.com>
2018-06-17 20:54:20 -0700

    initial commit

--- pages/qaHomePage.js
+++ pages/qaHomePage.js
@@ -4,28 +4,32 @@ import BasePage from './basePage';
 class QaHomePage extends BasePage {
     constructor() {
         super();
+        
         this.url = 'http://cnn.com';
 
         // include modules...
-        this.cnnLink = $('a#cnnLink');
+        //this.cnnLink = $('a#cnnLink');
 
-        // since the table happens to have multiple rows, the 5th row is clikced to show that there are at least 5 li links or rows:
+        // since the table happens to have multiple rows, the 5th row is clicked to show that there are at least 5 li links or rows:
         this.topStories = $('#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-2 > ul > li:nth-child(n+5)')
        
+
         this.StoriesList = $('#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-2 > ul')
         //         console.log('--------  StoriesList ---------')
         //         console.log('success')
         //         return this.StoriesList;
 
+
         // the first li link is clicked to show it has content and the link is functioning 
         this.storyKicker = $('#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul > li:nth-child(1) >  article > div > div.cd__content')
                    
-                   //return  this.storykicker.innerText;
-                   //console.log(storyKicker.innerText)
-                   //var text = element.textContent;
-                   //element.textContent = "this is some sample text";
-                   console.log('-------- StoryKicker ----------')
-                   //console.log(storykicker.innerText)
+                  console.log('-------- StoryKicker ----------')
+                  //return  this.storykicker.innerText;
+                  //console.log(storyKicker.innerText)
+                  //var text = element.textContent;
+                  //element.textContent = "this is some sample text";
+                  
+
     }
 
 
--- tests/qaTest.js
+++ tests/qaTest.js
@@ -2,37 +2,35 @@ import qaHomePage from '../pages/qaHomePage';
 import cnnPage from '../pages/cnnPage';
 import { Selector } from 'testcafe';
 
+
 fixture`CNN qaHomePage`
     .page`${qaHomePage.url}`
     .page`${cnnPage.url}`
 
-
-  test('1. Assert that there are at least 5 top stories', async t => {
+        test('1a. Assert that there are at least 5 top stories', async t => {
    
-     	await t
-     	.click(qaHomePage.topStories());
-		//.expect(qaHomePage.StoriesList.exists).ok();
-    	//.expect(qaHomePage.StoriesList.innerText != null);
+        await t.expect(qaHomePage.topStories.innerText != null);
+        //await t.expect(qaHomePage.topStories.exists).ok();
+        //await t.expect(qaHomePage.topStories.innerText).contains('');
+        await t.click(qaHomePage.topStories());
+    
+	});
+
+        test('1b. Assert that there are at least 5 top stories', async t => {
+     	
+        //.expect(list.count).eql(2)
+		await t.expect(qaHomePage.StoriesList.exists).ok();
+    	await t.expect(qaHomePage.StoriesList.innerText != null);
 	});
 
 
-
-// //////////////////////////////////////////////////////////////
-test('2a. Assert the kicker under the main story’s picture is filled out', async t => {
-        await t
-        
-    	//.click(qaHomePage.storyKicker());
-    	.expect(qaHomePage.storyKicker.innerText != null)
-    	//.expect(qaHomePage.storyKicker.innerText).ok()
-    	//.expect(qaHomePage.storyKicker.innerText).contains()
-    	// textContent   // .withText  // .innerText
-});
-
-test('2b. Assert the kicker under the main story’s picture is filled out', async t => {
-        await t
-        
-    	.click(qaHomePage.storyKicker());
-});
+        test('2. Assert the kicker under the main story’s picture is filled out', async t => {
+          
+    	await t.click(qaHomePage.storyKicker());
+    	await t.expect(qaHomePage.storyKicker.innerText != null);
+            console.log('kicker under the main story’s picture is filled out')
+               
+    });
 
 
 
